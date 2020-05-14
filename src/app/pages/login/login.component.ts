3394cfa3a868c0bf747d7d4d9ea2cead39747ac3
cafajardop <carlosafp84@hotmail.com>
2020-05-14 18:04:22 -0500

    Terminacion proyecto angular con firebase

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogin: UsuarioModel;
+  recordarme = false;
+
+  constructor(private auth: AuthService, 
+    private router: Router) { }
 
   ngOnInit() {
+    this.usuarioLogin = new UsuarioModel();
+    if(localStorage.getItem('email')){
+      this.usuarioLogin.email = localStorage.getItem('email');
+      this.recordarme = true;
+    }
   }
+
+  login(form: NgForm){
+    if(form.invalid){return;}
+
+    // console.log(this.usuarioLogin);
+    // console.log(form);
+    Swal.fire({
+      allowOutsideClick: false,
+      icon:'info',
+      text:'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.login(this.usuarioLogin)
+    .subscribe(resp =>{
+        console.log(resp);
+        Swal.close();
+        
+        if(this.recordarme){
+          localStorage.setItem('email', this.usuarioLogin.email);
+        }
+        
+        this.router.navigateByUrl('/home');
+    }, (err)=>{
+      console.log(err.error.error.message);
+      Swal.fire({
+        allowOutsideClick: false,
+        icon:'error',
+        title:'Error al autenticar',
+        text: err.error.error.message
+      });
+    })
+  }
+
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,104 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { UsuarioModel } from "../models/usuario.model";
+import { map } from "rxjs/operators";
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  private url = "https://identitytoolkit.googleapis.com/v1/accounts:";
+  private apikey = "AIzaSyDNauP2DA9dRVbbWwsYsPcTGx4Dh8tyohE";
+
+  userToken: string;
+
+  //Crear nuevo usuario
+  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  //Login
+  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+  constructor(private http: HttpClient) {
+    this.leerToken();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  login(usuario: UsuarioModel) {
+    const authData = {
+      // email:usuario.email,
+      // password:usuario.password,
+      ...usuario,
+      returnSecureToken: true,
+    };
+
+    return this.http
+      .post(`${this.url}signInWithPassword?key=${this.apikey}`, authData)
+      .pipe(
+        map((resp) => {
+          console.log("Entro en el mapa del RXJS");
+          this.guardarToken(resp["idToken"]);
+          return resp;
+        })
+      );
+  }
+
+  nuevoUsuario(usuario: UsuarioModel) {
+    const authData = {
+      // email:usuario.email,
+      // password:usuario.password,
+      ...usuario,
+      returnSecureToken: true,
+    };
+
+    return this.http
+      .post(`${this.url}signUp?key=${this.apikey}`, authData)
+      .pipe(
+        map((resp) => {
+          console.log("Entro en el mapa del RXJS");
+          this.guardarToken(resp["idToken"]);
+          return resp;
+        })
+      );
+  }
+  //Guardar en el local Storage
+  private guardarToken(idToken: string) {
+    this.userToken = idToken;
+    localStorage.setItem("token", idToken);
+    //para cuando expira el token
+    let hoy = new Date();
+    hoy.setSeconds(3600);
+
+    localStorage.setItem('expira', hoy.getDate().toString());
+
+  }
+  //Leemos el token
+  leerToken() {
+    if (localStorage.getItem("token")) {
+      this.userToken = localStorage.getItem("token");
+    } else {
+      this.userToken = "";
+    }
+    return this.userToken;
+  }
+
+  estaAutenticado(): boolean{
+
+    if(this.userToken.length < 2){
+      return false;
+    }
+
+    const expira = Number(localStorage.getItem('expira'));
+    const expiraDate = new Date();
+    expiraDate.setTime(expira);
+
+    if(expiraDate > new Date()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
